8b11a48135a287d4732dd3cc3cf927744c7dbc3f
IvanProkhorets <prohorets.ivan@gmail.com>
2022-06-09 13:41:26 +0300

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,15 @@
-const a = 'asdasd';
-console.log("🚀 ~ file: task-01.js ~ line 2 ~ a", a)
+// Number of categories: 3
+const itemEl = document.querySelectorAll('.item');
+console.log("Number of categories:", itemEl.length);
+
+// Category: Animals
+// Elements: 4
+
+
+// Category: Products
+// Elements: 3
+
+
+// Category: Technologies
+// Elements: 5
 
