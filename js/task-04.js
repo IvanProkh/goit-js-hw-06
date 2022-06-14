618ce12f7e2e39b9e456b0ddc606b0ea7b57925b
IvanProkhorets <prohorets.ivan@gmail.com>
2022-06-14 19:52:23 +0300

    update

--- js/task-04.js
+++ js/task-04.js
@@ -4,4 +4,28 @@
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
 
-const counterValue = 0;
+// const counterValue = 0;
+
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('[id="value"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+let counterValue = Number('0');
+counterValue = value.textContent;
+
+console.log("~ counterValue", counterValue)
+
+incrementBtn.addEventListener('click', () => {
+    console.log("~ counterValue", counterValue)
+    // counterValue = value.textContent;
+    return counterValue += 1;
+});
+
+decrementBtn.addEventListener('click', () => {
+    
+    console.log("~ counterValue", counterValue)
+    // counterValue = value.textContent;
+    return counterValue -= 1;
+});
+
+
