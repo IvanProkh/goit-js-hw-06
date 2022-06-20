6d94d18b2ec0585848688c4209b939d9f7bf833c
IvanProkhorets <prohorets.ivan@gmail.com>
2022-06-20 12:25:22 +0300

    completed task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bgColor: document.querySelector('.color'),
+  buttonChange: document.querySelector('.change-color'),
+}
+
+console.log("~ bgColor", refs.bgColor)
+console.log("~ buttonChange", refs.buttonChange)
+
+refs.buttonChange.addEventListener('click', onBgChangeColor);
+
+function onBgChangeColor(event) {
+
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.bgColor.textContent = getRandomHexColor();
+}
+
+
+
+
